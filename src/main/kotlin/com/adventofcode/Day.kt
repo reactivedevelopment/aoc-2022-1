c9b4aecf783d123e59ce97c907a3229807eba39f
Daniel Demidko <demidko@farpost.com>
2022-12-01 10:16:26 +0500

    all done

--- src/main/kotlin/com/adventofcode/Day.kt
+++ src/main/kotlin/com/adventofcode/Day.kt
@@ -1,16 +1,31 @@
 package com.adventofcode
 
+import com.adventofcode.Day.findMaxElfs
+
 object Day {
 
-  fun process(line: String) {
+  private val allElfs = mutableSetOf<Long>()
+  private var currElf = 0L
 
+  fun process(line: String) {
+    if (line.isEmpty()) {
+      allElfs.add(currElf)
+      currElf = 0L
+    } else {
+      currElf += line.toLong()
+    }
+  }
+
+  fun findMaxElfs(): Long {
+    return allElfs.sorted().takeLast(3).sum()
   }
 }
 
 fun main() {
   ::main
     .javaClass
-    .getResourceAsStream("/day.1.txt")!!
+    .getResourceAsStream("/input")!!
     .bufferedReader()
     .forEachLine(Day::process)
+  println(findMaxElfs())
 }
